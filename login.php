58e47248c6e9cb8381a3882a8755a7fac5d54cc3
alanlkw <100390836+alanlkw@users.noreply.github.com>
2023-06-04 10:34:29 +0800

    login.php

--- login.php
+++ login.php
@@ -2,13 +2,11 @@
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Get the form data
+    
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Validate and sanitize the input data
-
-    // Connect to the database (replace with your database credentials)
+   
     $servername = "localhost";
     $username = "your_username";
     $password = "your_password";
@@ -18,19 +16,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    // Retrieve the user's information from the database
+    
     $sql = "SELECT * FROM users WHERE username = '$username'";
     $result = $conn->query($sql);
 
     if ($result->num_rows === 1) {
         $row = $result->fetch_assoc();
-        // Verify the password
+        
         if (password_verify($password, $row['password'])) {
-            // Set session variables
+            
             $_SESSION['user_id'] = $row['id'];
             $_SESSION['username'] = $row['username'];
             $_SESSION['role'] = $row['role'];
-            header("Location: dashboard.php"); // Redirect to the dashboard or the desired page
+            header("Location: dashboard.php"); 
             exit();
         } else {
             echo "Invalid username or password";
