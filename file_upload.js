97fc39ab8b8fdaa3b0ec91c3b720a6d521680bd8
alanlkw <100390836+alanlkw@users.noreply.github.com>
2023-06-04 10:32:55 +0800

    file_upload.js

--- file_upload.js
+++ file_upload.js
@@ -1,5 +1,5 @@
 document.getElementById("file-upload-form").addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault(); 
 
     var fileInput = document.getElementById("file");
     var file = fileInput.files[0];
@@ -8,8 +8,7 @@ document.getElementById("file-upload-form").addEventListener("submit", function 
         var formData = new FormData();
         formData.append("file", file);
 
-        // You can use AJAX or fetch API to upload the file to the server
-        // Here, we'll just log the file details to the console
+        
         console.log("File Name: " + file.name);
         console.log("File Type: " + file.type);
         console.log("File Size: " + file.size + " bytes");
