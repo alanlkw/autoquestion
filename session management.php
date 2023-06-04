01b56d40ef7b8a3a2ecaf5acdedc31b826fe3fdc
alanlkw <100390836+alanlkw@users.noreply.github.com>
2023-06-04 10:34:47 +0800

    session management.php

--- session management.php
+++ session management.php
@@ -2,9 +2,9 @@
 session_start();
 
 if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php"); // Redirect to the login page if not logged in
+    header("Location: login.php"); 
     exit();
 }
 
-// Access user information using $_SESSION['user_id'], $_SESSION['username'], etc.
+
 ?>
