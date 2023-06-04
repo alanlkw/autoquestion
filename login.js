a9ef228a3aa8d47190c7d0bc693319c60a7bca5d
alanlkw <100390836+alanlkw@users.noreply.github.com>
2023-06-04 10:33:34 +0800

    login.js

--- login.js
+++ login.js
@@ -7,14 +7,10 @@ document.getElementById("login-form").addEventListener("submit", function(event)
     var username = usernameInput.value;
     var password = passwordInput.value;
 
-    // You can perform client-side validation here if required
-
-    // You can use AJAX or fetch API to send the login request to the server
-    // Here, we'll just log the entered username and password to the console
+    
     console.log("Username: " + username);
     console.log("Password: " + password);
 
-    // After sending the login request, you can redirect the user to the appropriate page
-    // Here, we'll just reload the current page
+    
     window.location.reload();
 });
