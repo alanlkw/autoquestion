9e5fbfddae06fa57fc95fcab2e260cbb79e579e8
alanlkw <100390836+alanlkw@users.noreply.github.com>
2023-06-04 10:23:50 +0800

    Update submit_question.js

--- submit_question.js
+++ submit_question.js
@@ -1,5 +1,5 @@
 document.getElementById("question-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault(); 
 
     var questionInput = document.getElementById("question");
     var answerInput = document.getElementById("answer");
@@ -7,12 +7,9 @@ document.getElementById("question-form").addEventListener("submit", function(eve
     var question = questionInput.value;
     var answer = answerInput.value;
 
-    // You can perform client-side validation here if required
-
-    // You can use AJAX or fetch API to send the question to the server
-    // Here, we'll just log the question and answer to the console
+    
     console.log("Question: " + question);
     console.log("Answer: " + answer);
 
-    // After submitting the question, you can redirect the user to a success page or perform any other necessary action
+  
 });
