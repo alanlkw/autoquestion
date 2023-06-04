60046dac5d089be79f8129f108d1e99c400e0991
alanlkw <100390836+alanlkw@users.noreply.github.com>
2023-06-04 10:30:09 +0800

    signup.js

--- signup.js
+++ signup.js
@@ -1,6 +1,5 @@
 document.getElementById("signup-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
-
+    event.preventDefault(); 
     var usernameInput = document.getElementById("username");
     var emailInput = document.getElementById("email");
     var passwordInput = document.getElementById("password");
@@ -9,13 +8,10 @@ document.getElementById("signup-form").addEventListener("submit", function(event
     var email = emailInput.value;
     var password = passwordInput.value;
 
-    // You can perform client-side validation here if required
-
-    // You can use AJAX or fetch API to send the signup data to the server
-    // Here, we'll just log the username, email, and password to the console
+    
     console.log("Username: " + username);
     console.log("Email: " + email);
     console.log("Password: " + password);
 
-    // After signing up, you can redirect the user to a success page or perform any other necessary action
+    
 });
